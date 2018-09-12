8e9cd4880750eedda1495e81124b6aa653f3a06f
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-09-12 16:36:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // Add code here
+  const rows = Number(prompt('how many rows would you like?'))
+  const columns = Number(prompt('how many columns would you like?'))
+  const board = document.getElementById('board');
+
+
+
+});

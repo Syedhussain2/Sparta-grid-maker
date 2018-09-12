f694b70235939ae7cf92669f4094a1fa1bc87796
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-09-12 18:21:40 +0100

    first stage

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
-  // Add code here
-  const rows = Number(prompt('how many rows would you like?'))
-  const columns = Number(prompt('how many columns would you like?'))
+
   const board = document.getElementById('board');
 
+  const rows = Number(prompt('how many rows would you like?'))
 
+  const columns = Number(prompt('how many columns would you like?'))
 
+  for (i = 0; i < rows; i++){
+  const row = document.createElement('div');
+  row.setAttribute('class', 'row'); // first arguement, what type do you wantt? so basically id your class example
+  board.appendChild(row);
+
+  for (j = 0; j < columns; j++){
+    const column = document.createElement('div');
+    column.setAttribute('class','col');
+    board.appendChild(column);
+  }
+}
 });
